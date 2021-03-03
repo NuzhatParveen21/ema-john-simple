17b28c41892862924b0f3cd237b57d292ae98411
Nuzhat Parveen <nuzhat.gisanalyst@gmail.com>
2021-03-03 14:19:08 -0500

    Ema-John-React

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,9 +5,10 @@ import './Product.css'
 
 
 const Product = (props) => {
-
     console.log(props);
-    const { name, img, seller, price, stock } = props.product;
+
+    const { name, img, seller, price, stock} = props.product;
+
     return (
         <div className="product">
             <div className="product-img">
@@ -18,7 +19,11 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p>only {stock} left in stock- order soon</p>
-                <button className="main-button"><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+                <button
+                    onClick={()=>props.handleAddProduct(props.product)}
+                    className="main-button">
+                    <FontAwesomeIcon icon={faShoppingCart} /> add to cart
+                </button>
             </div>
         </div>
 
